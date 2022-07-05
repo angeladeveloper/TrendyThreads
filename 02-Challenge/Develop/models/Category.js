f7bdf57bd31b7a7877d1d44e6544e461bcd23440
Angela W <squittenplease@gmail.com>
2022-07-04 22:10:16 -0700

    setting up files 📁

--- 02-Challenge/Develop/models/Category.js
+++ 02-Challenge/Develop/models/Category.js
@@ -2,11 +2,19 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    category_name: {
+      type: DataTypes.STRING
+    },
+    category_id: {
+      type: Seq.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
   },
   {
     sequelize,
