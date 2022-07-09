b8f892132c74190cd62261fdea02eca24a50070b
Angela W <squittenplease@gmail.com>
2022-07-09 16:27:56 -0700

    fix routes.... again.....

--- 02-Challenge/Develop/routes/api/category-routes.js
+++ 02-Challenge/Develop/routes/api/category-routes.js
@@ -35,15 +35,30 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const tagData = await Tag.create(req.body);
-    res.status(200).json(tagData);
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        category_id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 
@@ -52,7 +67,7 @@ router.delete('/:id', async (req, res) => {
   try {
     const categoryData = await Category.destroy({
       where: {
-        id: req.params.id,
+        category_id: req.params.id,
       },
     });
     if (!categoryData) {
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # TrendyThreads
-Backend online ecomerce site
+
+Execute CRUD functions on your backend ecomerce website
+
+## Tech Used
+
+- MySQL
+- Sequelize
+- Node.js
+- DotEnv
+
+## Features
+
+Manage your stock and information about your products with this simple to use api.
+
+## Changes to come
+
+- UI/UX
+- More data points for tracking
+- Price conversion
